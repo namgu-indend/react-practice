b09e8da4096a261d0302cd19c7e27c01aabd5914
namgu-indend <dev@indend.co.kr>
2020-08-20 12:54:44 +0900

    array first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import Counter from './Counter';
-import InputSample from './inputSample';
+import React, {useRef} from 'react';
+import UserList from './array/UserList';
+
+
 
 function App() {
+
+  const users = [
+    {
+      id: 1,
+      username: 'aa',
+      email: 'aa@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'bb',
+      email: 'bb@gmail.com',
+    },
+    {
+      id: 3,
+      username: 'cc',
+      email: 'cc@gmail.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current);
+    nextId.current += 1;
+  };
+
   return (
     <>
-      <InputSample></InputSample>
+      <UserList users={users}></UserList>
     </>
   );
 }
