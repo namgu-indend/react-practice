6af969c0026d13ab2e200d366231d8ab23e4adf2
namgu-indend <dev@indend.co.kr>
2020-08-20 14:21:24 +0900

    useEffect 기본 2

--- src/array/UserList.js
+++ src/array/UserList.js
@@ -3,19 +3,14 @@ import React, { useEffect } from "react";
 function User({ user, onRemove, onToggle }) {
     const { username, email, id, active } = user;
     
-    useEffect(() => {  // mount
-        console.log("컴포넌트가 화면에 나타남")
-        // props -> state
-        // REST API
-        // D3 Video.js
-        // setInterval, setTimeout
-        
-        return () => { // unmount
-            // clearInterval, clearTimeout
-            // 라이브러리 인스턴스 제거
-            console.log("컴포넌트가 화면에 사라짐")
+    useEffect(() => {
+        console.log("user 값이 설정됨 (insert, update)");
+        console.log(user);
+        return () => {
+            console.log("user 값이 바뀌기 전 (update전, delete)");
+            console.log(user);
         }
-    }, [])
+    }, [user])
 
     return (
         <div>
