00648f969fdf453efeaedc3046f73be6507664a8
namgu-indend <dev@indend.co.kr>
2020-08-20 13:46:31 +0900

    array push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './array/UserList';
-
-
+import CreateUser from './array/CreateUser';
 
 function App() {
 
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const {username, email} = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'aa',
@@ -21,18 +34,37 @@ function App() {
       username: 'cc',
       email: 'cc@gmail.com',
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
     console.log(nextId.current);
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    }
+    
+    // setUsers([...users, user]); // 기존항목은 복사하면서 넣으면서 새항목(user)를 추가
+    setUsers(users.concat(user)); // 두번째 방법 concat
+
+    setInputs({
+      username: '',
+      email: '',
+    })
     nextId.current += 1;
   };
 
   return (
     <>
-      <UserList users={users}></UserList>
+      <CreateUser 
+        username={username} 
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate} />
+
+      <UserList users={users} />
     </>
   );
 }
--- src/array/UserList.js
+++ src/array/UserList.js
@@ -9,8 +9,6 @@ function User({user}) {
 }
 
 function UserList( {users} ) {
-    
-
     return (
         <div>
             {
